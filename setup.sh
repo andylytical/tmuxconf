253c42daef1a75863a4e065a1ebb929ec7c8db8c
Andy Loftus <aloftus@gmail.com>
2017-08-11 16:20:24 -0500

    Add tmux.conf and setup

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+function die() {
+    echo "FATAL ERROR: $*"
+    exit 99
+}
+
+function continue_or_exit() {
+    local msg="Continue?"
+    [[ -n "$1" ]] && msg="$1"
+    echo "$msg"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes) return 0;;
+            No ) exit 1;;
+        esac
+    done
+}
+
+PGM="$0"
+PWD=$( pwd )
+SRCFN=tmux.conf
+TGTFN="${HOME}/.$SRCFN"
+
+# Attemp to find absolute path to file
+basedn=$( dirname "$PGM" )
+workdir=$( readlink -e "$basedn" )
+abs_fn="${workdir}/$SRCFN"
+[[ -f "$abs_fn" ]] || die "Unable to find absolute path for source $SRCFN"
+
+# Check for existing target
+[[ -f "$TGTFN" ]] \
+&& continue_or_exit "Can I overwrite your existing $SRCFN file?"
+rm -f "$TGTFN"
+
+# Create symlink 
+ln -s "$abs_fn" "$TGTFN"
