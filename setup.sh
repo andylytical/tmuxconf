148645073bd344104c4ed852932d9f2d4d37eba5
Andy Loftus <aloftus@gmail.com>
2020-09-20 00:58:10 -0500

    Split config; Alt-vi keys for pane switching

--- setup.sh
+++ setup.sh
@@ -17,21 +17,30 @@ function continue_or_exit() {
     done
 }
 
+set -x
+
 PGM="$0"
 PWD=$( pwd )
+TS=$(date +%s)
 SRCFN=tmux.conf
 TGTFN="${HOME}/.$SRCFN"
+SRC_CONF_DIR=tmux/config
+TGT_CONF_DIR="${HOME}/.$SRC_CONF_DIR"
 
-# Attemp to find absolute path to file
+# Attempt to find absolute path to file
 basedn=$( dirname "$PGM" )
 workdir=$( readlink -e "$basedn" )
 abs_fn="${workdir}/$SRCFN"
 [[ -f "$abs_fn" ]] || die "Unable to find absolute path for source $SRCFN"
 
-# Check for existing target
-[[ -f "$TGTFN" ]] \
-&& continue_or_exit "Can I overwrite your existing $SRCFN file?"
-rm -f "$TGTFN"
+# Install conf file
+install -vbC -S "$TS" "$SRCFN" "$TGTFN"
 
-# Create symlink 
-ln -s "$abs_fn" "$TGTFN"
+# Install config files
+mkdir -p "$TGT_CONF_DIR"
+install -vbC -S "$TS" -t "$TGT_CONF_DIR" "$SRC_CONF_DIR/"*
+#find "$SRC_CONF_DIR" -type f -print \
+#| while read; do
+#    fn=$( basename "$REPLY" )
+#    install -vbC -S "$TS" -T "$REPLY" "$TGT_CONF_DIR/$fn"
+#done
